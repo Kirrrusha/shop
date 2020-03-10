1812827a734f83e0a888458d5fc93d64f3e0f9ef
kirrrusha <kirilllebedenko@gmail.com>
2020-03-10 13:11:30 +0300

    fix old code

--- src/Blocks/Header/Header.js
+++ src/Blocks/Header/Header.js
@@ -9,35 +9,17 @@ import BurgerMenu from './BurgerMenu'
 
 export default class Header extends Component {
 
-//   checkIfModalShow = () => {
-//     let localDate =  localStorage.getItem('xusha')
-//     let compareDate = Date.now() - (1000*3600*24) // minus 24h
-//     let result = true;    
-  
-//   if (typeof localDate === 'string') {
-//     localDate = parseInt(localDate)
-//       if (localDate > compareDate) {
-//         result = false; 
-//     } 
-//   }
-//    return result;
-//  }
-
   render() {
-    // let modal = '';   
-    // if (this.checkIfModalShow()) {			
-    //     modal = <Modal />;
-    // }   
 
     return (
       <header>
         <div className='wrapper'>
           <div className='header-blocks'>
-            
+
             <a href='/'>
               <img className='header-logo' src={Logo} alt='#' />
             </a>
-           
+
             <Menu />
             <BurgerMenu />
           </div>
