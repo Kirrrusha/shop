b52da31f90bfcdb4df25e7b173f125496b551e02
inan <ivan@sahaja-yoga.vm>
2020-03-16 10:09:45 +0200

    remove disabled code

--- src/Components/Pages/Product.js
+++ src/Components/Pages/Product.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-// import parseHtml from 'html-react-parser';
 import BlockProduct from '../../Blocks/Product/Product';
 
 import '../../assets/css/Product.scss';
