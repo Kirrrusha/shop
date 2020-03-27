512d72d396b9c280afa43a1dc97b0dc3196f4e22
kirrrusha <kirilllebedenko@gmail.com>
2020-03-27 12:38:37 +0300

    flow categories list

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import {connectRouter } from 'connected-react-router';
 import authReducer from '../modules/auth';
+import categories from '../modules/categories';
 
 import {history} from '../history';
 
 const rootReducer = combineReducers({
     router: connectRouter(history),
     auth: authReducer,
+    categories,
 });
 
 export default rootReducer;
