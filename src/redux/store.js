db7b4eb1b31a7e07bd7def59bac0817efe341123
kirrrusha <kirilllebedenko@gmail.com>
2020-04-12 13:27:34 +0300

    fix routes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,10 @@
 import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
-import {routerMiddleware} from 'connected-react-router';
-
-import {history} from './history';
 import rootReducer from './reducers';
 
 const middleWares = [
   thunk,
-  routerMiddleware(history)
 ];
 
 const composeEnhancers = composeWithDevTools(
