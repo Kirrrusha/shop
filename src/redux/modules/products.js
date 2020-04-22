dcf5956293a7902550ad6676aa0ee28c99950054
kirrrusha <kirilllebedenko@gmail.com>
2020-04-22 23:38:24 +0300

    add product page redux flow

--- src/redux/modules/products.js
+++ src/redux/modules/products.js
@@ -1,74 +1,93 @@
 import axios from 'axios';
 import {API_HTTP} from '../../configs/environment';
+import {createSelector} from 'reselect';
 
-const ActionTypesProductsByCategoryId = {
-    PRODUCTS_REQUEST: 'PRODUCTS_BY_CATEGORY_REQUEST',
-    PRODUCTS_SUCCESS: 'PRODUCTS_BY_CATEGORY_SUCCESS',
-    PRODUCTS_FAILURE: 'PRODUCTS_BY_CATEGORY_FAILURE'
-  };
-  
-  const initialProductsState = {
-    list: [],
-    pending: {
-      productsList: false
-    },
-    errors: null,
-  };
+const ActionTypesProducts = {
+  GET_PRODUCTS_REQUEST: 'GET_PRODUCTS_REQUEST',
+  GET_PRODUCTS_SUCCESS: 'GET_PRODUCTS_SUCCESS',
+  GET_PRODUCTS_FAILURE: 'GET_PRODUCTS_FAILURE',
+};
+
+const initialProductsState = {
+  list: [],
+  pending: {
+    productsList: null,
+  },
+  errors: null
+};
 
 
-  export default function (state = initialProductsState, action) {
-    const {PRODUCTS_REQUEST, PRODUCTS_SUCCESS, PRODUCTS_FAILURE} = ActionTypesProductsByCategoryId;
-    switch (action.type) {
-  
-      case PRODUCTS_REQUEST:
-        return {
-          ...state,
-          pending: {
-            ...state.pending,
-            productsList: true
-          }
-        };
-  
-      case PRODUCTS_SUCCESS:
-        return {
-          ...state,
-          list: action.payload,
-          pending: {
-            ...state.pending,
-            productsList: false
-          }
-        };
-      case PRODUCTS_FAILURE:
-        return {
-          ...state,
-          errors: action.errors,
-          pending: {
-            ...state.pending,
-            productsList: false
-          }
-        };
-      default:
-        return state;
-    }
+export default function (state = initialProductsState, action) {
+  const {
+    GET_PRODUCTS_REQUEST, GET_PRODUCTS_SUCCESS, GET_PRODUCTS_FAILURE,
+  } = ActionTypesProducts;
+  switch (action.type) {
+
+    case GET_PRODUCTS_REQUEST:
+      return {
+        ...state,
+        pending: {
+          ...state.pending,
+          productsList: true
+        }
+      };
+
+    case GET_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        list: action.payload,
+        pending: {
+          ...state.pending,
+          productsList: false
+        }
+      };
+    case GET_PRODUCTS_FAILURE:
+      return {
+        ...state,
+        errors: action.errors,
+        pending: {
+          ...state.pending,
+          productsList: false
+        }
+      };
+
+    default:
+      return state;
   }
-  
-  export const getProductsByCategory = (categoryId) => dispatch => {
-    dispatch({
-      type: ActionTypesProductsByCategoryId.PRODUCTS_REQUEST
-    });
-  
-    axios
-      .get(`${API_HTTP}/api/v1/products/byCategory/${categoryId}`)
-      .then(({data}) => { 
-        //console.log(data)
-       dispatch({
-        type: ActionTypesProductsByCategoryId.PRODUCTS_SUCCESS,
-        payload: data      
-      })    
+}
+
+//actions
+export const getAllProducts = () => dispatch => {
+  dispatch({
+    type: ActionTypesProducts.GET_PRODUCTS_REQUEST
+  });
+
+  axios
+    .get(`${API_HTTP}/api/v1/products`)
+    .then(({data}) => {
+      dispatch({
+        type: ActionTypesProducts.GET_PRODUCTS_SUCCESS,
+        payload: data
+      });
     })
-      .catch((errors) =>
-        dispatch({
-          type: ActionTypesProductsByCategoryId.PRODUCTS_FAILURE,
-          errors
-        }));     
-  };
+    .catch((errors) =>
+      dispatch({
+        type: ActionTypesProducts.GET_PRODUCTS_FAILURE,
+        errors
+      }));
+};
+
+//selectors
+const selectProducts = state => state.products.list;
+const selectProductId = (state, itemId) => itemId;
+
+export const selectProductsByCategoryId = createSelector(
+  [selectProducts, selectProductId],
+  (products, productId) => products.find(product => product.status && product.productId === +productId)
+);
+
+export const selectShortProductList = createSelector(selectProducts,
+  (products) => products.map(({name, updatedAt, status, id, productId}) =>
+    ({name, updatedAt, status, id, productId})
+  )
+)
