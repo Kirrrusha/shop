cb590dfdf77f369b287b9db734893dd893d11ede
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-22 20:57:43 +0000

    Deploy Kirrrusha/shop to github.com/Kirrrusha/shop.git:gh-pages

---
+++ precache-manifest.87465106cfad374a4c475ff1273d0e49.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e588c0fbca33858e882edbc1acb0db07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a464ae04f5568b0c533",
+    "url": "/static/css/main.bf469e8a.chunk.css"
+  },
+  {
+    "revision": "242dd1132d4849e8bd7c",
+    "url": "/static/js/2.1ba57a64.chunk.js"
+  },
+  {
+    "revision": "b5d23eb95f12f07608aa002de8d8de17",
+    "url": "/static/js/2.1ba57a64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a464ae04f5568b0c533",
+    "url": "/static/js/main.5d8a2439.chunk.js"
+  },
+  {
+    "revision": "b2c14794cc0a772622b6",
+    "url": "/static/js/runtime-main.27138ad5.js"
+  },
+  {
+    "revision": "a361f0ccb74ec4738fc9cf06d4f8883c",
+    "url": "/static/media/Montserrat-Bold.a361f0cc.eot"
+  },
+  {
+    "revision": "a79b68fcda60ce8f337f51d3220ef2a5",
+    "url": "/static/media/Montserrat-Bold.a79b68fc.woff"
+  },
+  {
+    "revision": "bf2c26d2b14e3fc3b9f065fb308866c8",
+    "url": "/static/media/Montserrat-Bold.bf2c26d2.ttf"
+  },
+  {
+    "revision": "1de7f0493a20873dd93aea5625fb1b40",
+    "url": "/static/media/Montserrat-Light.1de7f049.eot"
+  },
+  {
+    "revision": "4fff33ac95aaed9f68e1471c805558ec",
+    "url": "/static/media/Montserrat-Light.4fff33ac.woff"
+  },
+  {
+    "revision": "ba044963492ff3ec7755d1117661a360",
+    "url": "/static/media/Montserrat-Light.ba044963.ttf"
+  },
+  {
+    "revision": "08a754d1d68f4671eab5e11ece77cd03",
+    "url": "/static/media/Montserrat-Regular.08a754d1.ttf"
+  },
+  {
+    "revision": "2dd0a1de870af34d48d43b7cad82b8d9",
+    "url": "/static/media/Montserrat-Regular.2dd0a1de.eot"
+  },
+  {
+    "revision": "de4a911e831ca01751ab001a54de34dd",
+    "url": "/static/media/Montserrat-Regular.de4a911e.woff"
+  },
+  {
+    "revision": "143850af7ce7bad6c8012715f25c10f9",
+    "url": "/static/media/Montserrat-Thin.143850af.eot"
+  },
+  {
+    "revision": "2597559f3f979e5d8bd947eac5a94af7",
+    "url": "/static/media/Montserrat-Thin.2597559f.ttf"
+  },
+  {
+    "revision": "7877090582a078cf7ea3b69eae355d35",
+    "url": "/static/media/Montserrat-Thin.78770905.woff"
+  },
+  {
+    "revision": "1102569119864d07d87dac06810f0622",
+    "url": "/static/media/back-img-mobile.11025691.png"
+  },
+  {
+    "revision": "a521f3e38cb23b31ae70baad1d7194eb",
+    "url": "/static/media/cart.a521f3e3.svg"
+  },
+  {
+    "revision": "fae3fff7f19a159771859a9f0719fccb",
+    "url": "/static/media/product1.fae3fff7.png"
+  },
+  {
+    "revision": "64a81b410cbb564a97e5ed113d895f74",
+    "url": "/static/media/product2.64a81b41.png"
+  },
+  {
+    "revision": "4a338e1a3c05450f16601feb63da6721",
+    "url": "/static/media/product3.4a338e1a.png"
+  },
+  {
+    "revision": "7ff305d20ed8cb062385c14276390613",
+    "url": "/static/media/product4.7ff305d2.png"
+  },
+  {
+    "revision": "aa9faa22df5a18cb211d819e10c94d65",
+    "url": "/static/media/search.aa9faa22.svg"
+  },
+  {
+    "revision": "37cf26578119757de5727868e770a13e",
+    "url": "/static/media/view.37cf2657.svg"
+  }
+]);
